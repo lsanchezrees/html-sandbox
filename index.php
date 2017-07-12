ab56f9a27389f0ee0a62a23c703e4cb7dde9dda3
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-12 13:59:56 -0600

    Added table and lists.

--- index.php
+++ index.php
@@ -6,5 +6,43 @@
 	</head>
 	<body>
 		<h1>This is my web page.</h1>
+
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<!-- HTML comment -->
+		<h2>A few of my favorite things</h2>
+		<ul>
+			<li>Brian</li>
+			<li>Bailey</li>
+			<li>Lucy</li>
+			<li>Hiking</li>
+			<li>Chocolate</li>
+			<li>Art</li>
+		</ul>
+
+		<h2>Things to do today</h2>
+		<ol>
+			<li>Clean the breakfast dishes</li>
+			<li>Call Brian</li>
+			<li>Get all my stuff together</li>
+			<li>Never eat doughnuts again</li>
+		</ol>
 	</body>
 </html>
